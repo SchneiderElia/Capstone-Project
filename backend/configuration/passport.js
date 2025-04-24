6403ea886204a4b1a12dbbfee3d2efbe3c58d2c5
Elia Schneider <elyssch@gmail.com>
2025-04-25 00:28:41 +0200

    fix

--- backend/configuration/passport.js
+++ backend/configuration/passport.js
@@ -8,7 +8,7 @@ const googleStrategy = new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     //callbackURL: 'http://localhost:4000/api/v1/auth/google/callback'
-    callbackURL: process.env.SERVER_FRONTEND + '/auth/google/callback',
+    callbackURL: process.env.SERVER_BACKEND + '/auth/google/callback',
     //process.env.SERVER_FRONTEND + process.env.API_VERSION + process.env.GOOGLE_CALLBACK_URL,
   },
 
